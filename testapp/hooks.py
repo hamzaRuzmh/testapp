4f6e3264203d7de607e9cd770fb610514eeb85de
hamzaRuzmh <erpnexthamza@gmail.com>
2025-01-11 18:06:50 +0530

    App In

--- testapp/hooks.py
+++ testapp/hooks.py
@@ -241,4 +241,7 @@ app_license = "mit"
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
-fixtures=['Custom Field']
+fixtures=[
+  'Custom Field',
+ { "dt":"DocType","filters":[["name","=","honda"]]}
+  ]
